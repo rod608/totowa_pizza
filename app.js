a564ef550cec10df6a8d61a4fc03bc61cf2abe98
rod608 <rod608ney@gmail.com>
2022-05-19 19:53:23 -0400

    Added some nice mobile nav features.

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-// Variables: Query Selector for menu button && menuOpen boolean flag
+/* Custom Scripts */
 menuBtn = document.querySelector('.navbar-toggler');
+navbar = document.querySelector('nav');
 let menuOpen = false;
 
-// Event Listener 4 click
+// 
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
+        navbar.classList.add('bg-white');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
+        navbar.classList.remove('bg-white');
         menuOpen = false;
     }
+});
+
+/* Mobile: Automatically close navbar dropdown once an option is selected. */
+
+// Step #1: Make query selectors for all nav items.
+navItems = document.querySelectorAll('.nav-item');
+
+// Step #2: Add an event listener that listens for a click
+navItems.forEach(navItem => {
+    navItem.addEventListener('click', () => {
+        if (menuOpen) {
+            setTimeout(() => {menuBtn.click()}, '800');
+        }
+    });
 });
